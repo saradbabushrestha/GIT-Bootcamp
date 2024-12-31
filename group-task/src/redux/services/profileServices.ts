ffac9fa4888b16cc5aa2b23211bfdf7cd6821294
shubhaaaaaaa <shubhakhadgi007@gmail.com>
2024-12-31 10:23:12 +0545

    added endpoints.ts

--- group-task/src/redux/services/profileServices.ts
+++ group-task/src/redux/services/profileServices.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
-
-const API_URL = "http://localhost:5001/users";
+import { endpoints } from "../endpoints";
 
 export const getUserDetails = async (userId: string) => {
-    try {
-      const response = await axios.get(`${API_URL}/${userId}`);
-      return response.data; 
-    } catch (e) {
-      console.log("Error encountered:", e);
-    }
+  try {
+    const response = await axios.get(endpoints.users.getById(userId));
+    return response.data;
+  } catch (e) {
+    console.log("Error", e);
+  }
 };
